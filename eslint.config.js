617e0cda6ad42ba8707e0ed7eb1547fc62f99db6
madao <gn00678465@gmail.com>
2025-05-07 16:43:08 +0800

    chore: 加入 eslint 設定

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  react: true,
+  stylistic: true,
+  typescript: true,
+  ignores: [
+    'src/components/ui/*',
+    'src/lib/utils.ts',
+  ],
+})
